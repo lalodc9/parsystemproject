389e5786e8e4be5eb46c935c02d1e9bed0281ffd
Cynthia Teran <al226582@edu.uaa.mx>
2021-09-13 14:03:28 -0500

    Esqueleto de cambio de vista y registro

---
+++ cambioVista.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    </head>
+    <body>
+        <div>
+            <h1>UPS! PARECE SER QUE NO ERES TUTOR PAR</h1>
+            <a href="">REGÍSTRATE COMO TUTOR PAR AQUÍ</a><br>
+            <button>REGRESAR</button>
+        </div>
+</html>
+
