1a2ceb51092f782e50f543a12b5c18eec5048b84
lalodc9 <al269314@edu.uaa.mx>
2021-09-13 14:32:36 -0500

    Archivo programarAsesoria

---
+++ programarAsesoria.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/estilos.css">
+        <meta http-equiv=”Content-Type” content=”text/html; charset=UTF-8″ />
+    </head>
+    <body>
+        <img src="img/logouaa.png" id="logouaa">
+        <div>
+            <h1>MÓDULO DE ASESORÍAS</h1>
+        </div>
+
+        <ul>
+            <li>Solicitudes</li>
+            <li>En proceso</li>
+            <li>Próximas asesorías</li>
+        </ul>
+
+        <div class="principal">
+            
+            <div>
+                <img src="img/avatar.png">
+                <p>Nombre</p>
+                <p><?php 
+                    echo "ABEL SOTO VALDEZ";
+                ?>
+                </p>
+                <p>Contacto</p>
+                <p><?php 
+                    echo "449 283 4903 / abel@gmail.com\n";
+                    echo "ESTRUCTURAS DE DATOS"
+                ?>
+                <button>CANCELAR</button>
+                </p>
+            </div>
+            
+            <br>
+            <div>
+                <img src="img/avatarunknown.png">
+                <p><?php 
+                    echo "ESPERANDO RESPUESTA\n";
+                    echo "ÁLGEBRA LINEAL";
+                ?>
+                </p>
+                <button>CANCELAR</button>
+            </div>
+        </div>
+        <a href="">REGÍSTRATE COMO TUTOR PAR AQUÍ</a>
+
+    </body>
+</html>
