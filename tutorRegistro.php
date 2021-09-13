61e33edb7097ab4701b052022832eee0798a9ec8
lalodc9 <al269314@edu.uaa.mx>
2021-09-13 15:16:10 -0500

    Estructura de tutorRegistro

--- tutorRegistro.php
+++ tutorRegistro.php
@@ -12,22 +12,72 @@
         </div>
 
         <img src="img/logogallo.png" id="logogallo">
-
-        <form class="formulariosregistro">
-            <label for="id" id="textId">ID</label>
-            <input type="text" id="id" name="id" pattern="al+[0-9]{6}" required><br><br>
-            <label for="password" id="textPasswoord">CONTRASEÑA</label> 
-            <input type="password" id="password" name="password"><br><br>
-            <a href="http://www.pixelania.com" id="register">REGÍSTRATE</a><br><br>
-            <input type="submit" value="INGRESAR">
-        </form>
-        <p>Nota: El ID es el proporcionado por la UNIVERSIDAD</p>
-
-        <?php
-            include("php/conexion.php");
-            conn();
-        ?>
-
+        
+        <div>
+            
+            
+            <form>
+                <hr>
+                <h2>INFORMACIÓN GENERAL</h2><br>
+                <hr>
+                Promedio Inmediato Anterior: <input type="text"><br>
+                <hr>
+                <h2>INFORMACIÓN DE TUTORÍA</h2>
+                <hr>
+                Materias a Impartir:
+                <select>
+                    <option><?php echo "Matemáticas Discretas" ?></option>
+                    <option><?php echo "Sistemas Operativos" ?></option>
+                    <option><?php echo "Unix" ?></option>
+                </select>
+                <br>
+                Promedio de la materia:
+                <input type="text">
+                <button>AGREGAR MATERIA</button>
+                <table border="2">
+                    <tr>
+                        <th>MATERIA</th>
+                        <th>PROMEDIO</th>
+                    </tr>
+                    <tr>
+                        <td>
+                            <?php 
+                                echo "Matemáticas Discretas"
+                            ?>
+                        </td>
+                        <td>
+                            <?php 
+                                echo "9.8"
+                            ?>
+                        </td>
+                    <tr>
+                    <tr>
+                       
+                        <td>
+                            <?php 
+                                echo "Unix"
+                            ?>
+                        </td>
+                        <td>
+                            <?php 
+                                echo "9.2"
+                            ?>
+                        </td>
+                    </tr>
+                </table>
+                <br>
+                Programa <input type="radio" name="program"> Servicio Social <input type="radio" name="program"> Voluntariado 
+                <br>
+                <hr>
+                <h2>INFORMACIÓN DE CONTACTO</h2>
+                <hr>
+                Correo Electrónico Alternativo <input type="text">
+                Número de teléfono <input type="text">
+                Selecciona tu foto de perfil <input type="text">
+            </form>
+        </div>
+        
+        
 
     </body>
 </html>
